6cab5b6a47fd5a865cbe3876e27d9f7fe9f3b91a
Akshay Ghadavale <52067529+AkshayGhadavale@users.noreply.github.com>
2023-07-11 08:28:17 +0530

    Update return_in_function.js

--- return_in_function.js
+++ return_in_function.js
@@ -1,1 +1,20 @@
 
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+
+function add()
+{
+    let b=0;
+for (let i=0;i<arguments.length;i++)
+{
+b = b + arguments[i];
+
+  
+}
+return b;
+}
+
+let c=add(4,8);
+
+console.log(c);
